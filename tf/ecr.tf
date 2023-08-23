b4aaf7bfaf1e320776f43458e6d7f4e3f7b0ca82
Rodrigo Curiel <rodrigocuriel95@gmail.com>
2023-08-23 16:10:24 -0700

    changed repo_name to name

--- tf/ecr.tf
+++ tf/ecr.tf
@@ -1,5 +1,5 @@
 resource "aws_ecr_repository" "drigo_transcripts" {
-  repository_name = "drigo/transcripts"
+  name = "drigo/transcripts"
   image_scanning_configuration {
     scan_on_push = true
   }
